7451d73a240860bc128cc16112ee9b548cd9eb81
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2024-10-08 21:28:11 -0400

    initial commit

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+const API_URL = "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api";
+
+/*TODO LOGIN /POST*/
+
+/*TODO REGISTER /POST */
+
+/*TODO ALL BOOKS /GET */
+
+/*TODO SINGLE BOOKS /GET */
